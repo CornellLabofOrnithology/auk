2af2c02cdd710aff09836da168a4a14195da04c4
Matt Strimas-Mackey <mstrimas@gmail.com>
2017-07-12 09:22:29 -0400

    update data documentation for new taxonomy fields

--- R/data.r
+++ R/data.r
@@ -7,8 +7,8 @@
 #' Non-ASCII characters (e.g. those with accents) have been converted to ASCII 
 #' equivalents in this data frame.
 #'
-#' @format A data frame with six variables and 15,251 rows:
-#'
+#' @format A data frame with eight variables and 15,251 rows:
+#' - `taxon_order`: numeric value used to sort rows in taxonomic order.
 #' - `category`: whether the entry is for a species or another
 #' field-identifiable taxon, such as `spuh`, `slash`, `hybrid`, etc.
 #' - `species_code`: a unique alphanumeric code identifying each species.
@@ -17,6 +17,8 @@
 #' - `order`: the scientific name of the order that the species belongs to.
 #' - `family`: the family of the species, in the form "Parulidae (New World
 #' Warblers)".
+#' - `report_as`: for subspecies and recognizable forms, this field links to the 
+#' corresponding species code.
 #'
 #' For further details, see \url{http://help.ebird.org/customer/en/portal/articles/1006825-the-ebird-taxonomy}
 #'
