68eb4dc60a7acd0c4afc08eab82997583d1d8ce7
Matt Strimas-Mackey <mstrimas@gmail.com>
2020-10-16 06:21:18 -0700

    one more broken link fixed

--- makefile.r
+++ makefile.r
@@ -1,23 +1,23 @@
-  # clean up
-  unlink(list.files("man", full.names = TRUE))
-  devtools::clean_vignettes()
-  pkgdown::clean_site()
-  
-  # rebuild docs and install
-  devtools::document()
-  devtools::install_local(force = TRUE)
-  
-  # local tests and checks
-  devtools::test()
-  devtools::check()
-  
-  # vignettes, readme, site
-  rmarkdown::render("README.Rmd")
-  devtools::build_vignettes()
-  pkgdown::build_site()
-  dir.create("docs/cheatsheet/")
-  file.copy("cheatsheet/auk-cheatsheet.png", "docs/cheatsheet/auk-cheatsheet.png")
-  
+# clean up
+unlink(list.files("man", full.names = TRUE))
+devtools::clean_vignettes()
+pkgdown::clean_site()
+
+# rebuild docs and install
+devtools::document()
+devtools::install_local(force = TRUE)
+
+# local tests and checks
+devtools::test()
+devtools::check()
+
+# vignettes, readme, site
+rmarkdown::render("README.Rmd")
+devtools::build_vignettes()
+pkgdown::build_site()
+dir.create("docs/cheatsheet/")
+file.copy("cheatsheet/auk-cheatsheet.png", "docs/cheatsheet/auk-cheatsheet.png")
+
 # checks
 devtools::check_win_devel()
 devtools::check_win_release()
