f4008d5f7459f2cec3503e51a9ddf1a790728056
Matt Strimas-Mackey <mes335@cornell.edu>
2023-11-11 10:24:00 -0800

    use standard paths on windows

--- tests/testthat/test_set-env.R
+++ tests/testthat/test_set-env.R
@@ -2,6 +2,6 @@ test_that("auk_set_ebd_path()", {
   renv <- file.path(temp_dir, ".Renviron")
   Sys.setenv(R_ENVIRON_USER=renv)
   returned_path <- auk_set_ebd_path(temp_dir)
-  expect_equal(returned_path, normalizePath(temp_dir))
+  expect_equal(returned_path, normalizePath(temp_dir, winslash = "/"))
   expect_equal(returned_path, Sys.getenv("EBD_PATH"))
 })
